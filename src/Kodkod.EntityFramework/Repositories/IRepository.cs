85ec21bea9f7a109213194d2800d29c334df640e
alirizaadiyahsi <alirizaadiyahsi@outlook.com>
2018-05-25 23:32:17 +0300

    Added other necessary repository methods

--- src/Kodkod.EntityFramework/Repositories/IRepository.cs
+++ src/Kodkod.EntityFramework/Repositories/IRepository.cs
@@ -2,23 +2,108 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading;
 using System.Threading.Tasks;
+using Kodkod.Core.PagedLists;
 using Microsoft.EntityFrameworkCore.Query;
 
 namespace Kodkod.EntityFramework.Repositories
 {
     public interface IRepository<TEntity> where TEntity : class
     {
-        Task<List<TEntity>> GetAllAsync(
+        void ChangeTable(string table);
+
+        IPagedList<TEntity> GetPagedList(Expression<Func<TEntity, bool>> predicate = null,
+                                         Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+                                         Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+                                         int pageIndex = 0,
+                                         int pageSize = 20,
+                                         bool disableTracking = false);
+
+        Task<IPagedList<TEntity>> GetPagedListAsync(Expression<Func<TEntity, bool>> predicate = null,
+                                                    Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+                                                    Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+                                                    int pageIndex = 0,
+                                                    int pageSize = 20,
+                                                    bool disableTracking = false,
+                                                    CancellationToken cancellationToken = default(CancellationToken));
+
+        IPagedList<TResult> GetPagedList<TResult>(Expression<Func<TEntity, TResult>> selector,
+                                                  Expression<Func<TEntity, bool>> predicate = null,
+                                                  Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+                                                  Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+                                                  int pageIndex = 0,
+                                                  int pageSize = 20,
+                                                  bool disableTracking = false) where TResult : class;
+
+        Task<IPagedList<TResult>> GetPagedListAsync<TResult>(Expression<Func<TEntity, TResult>> selector,
+                                                             Expression<Func<TEntity, bool>> predicate = null,
+                                                             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+                                                             Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+                                                             int pageIndex = 0,
+                                                             int pageSize = 20,
+                                                             bool disableTracking = false,
+                                                             CancellationToken cancellationToken = default(CancellationToken)) where TResult : class;
+
+        TEntity GetFirstOrDefault(Expression<Func<TEntity, bool>> predicate = null,
+                                  Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+                                  Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+                                  bool disableTracking = false);
+
+        TResult GetFirstOrDefault<TResult>(Expression<Func<TEntity, TResult>> selector,
+                                           Expression<Func<TEntity, bool>> predicate = null,
+                                           Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+                                           Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+                                           bool disableTracking = false);
+
+        Task<TResult> GetFirstOrDefaultAsync<TResult>(Expression<Func<TEntity, TResult>> selector,
             Expression<Func<TEntity, bool>> predicate = null,
             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
             Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
             bool disableTracking = false);
 
-        Task<TEntity> GetFirstOrDefaultAsync(
-            Expression<Func<TEntity, bool>> predicate = null,
+        Task<TEntity> GetFirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate = null,
             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
             Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
             bool disableTracking = false);
+
+        IQueryable<TEntity> FromSql(string sql, params object[] parameters);
+
+        TEntity Find(params object[] keyValues);
+
+        Task<TEntity> FindAsync(params object[] keyValues);
+
+        Task<TEntity> FindAsync(object[] keyValues, CancellationToken cancellationToken);
+
+        [Obsolete("This method is not recommended, please use GetPagedList or GetPagedListAsync methods")]
+        IQueryable<TEntity> GetAll();
+
+        int Count(Expression<Func<TEntity, bool>> predicate = null);
+
+        void Insert(TEntity entity);
+
+        void Insert(params TEntity[] entities);
+
+        void Insert(IEnumerable<TEntity> entities);
+
+        Task InsertAsync(TEntity entity, CancellationToken cancellationToken = default(CancellationToken));
+
+        Task InsertAsync(params TEntity[] entities);
+
+        Task InsertAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default(CancellationToken));
+
+        void Update(TEntity entity);
+
+        void Update(params TEntity[] entities);
+
+        void Update(IEnumerable<TEntity> entities);
+
+        void Delete(object id);
+
+        void Delete(TEntity entity);
+
+        void Delete(params TEntity[] entities);
+
+        void Delete(IEnumerable<TEntity> entities);
     }
 }
--- src/Kodkod.EntityFramework/Repositories/Repository.cs
+++ src/Kodkod.EntityFramework/Repositories/Repository.cs
@@ -2,30 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
+using Kodkod.Core.PagedLists;
+using Kodkod.Core.PagedLists.Extensions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Query;
 
 namespace Kodkod.EntityFramework.Repositories
 {
-    public class Repository<TEntity> : IRepository<TEntity>
-        where TEntity : class
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
-        protected readonly KodkodDbContext DbContext;
+        protected readonly DbContext DbContext;
         protected readonly DbSet<TEntity> DbSet;
 
-        public Repository(KodkodDbContext dbContext)
+        public Repository(DbContext dbContext)
         {
-            DbContext = dbContext;
+            DbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
             DbSet = DbContext.Set<TEntity>();
         }
 
-        //todo: change this to paged list
-        public async Task<List<TEntity>> GetAllAsync(
+        public void ChangeTable(string table)
+        {
+            if (DbContext.Model.FindEntityType(typeof(TEntity)).Relational() is RelationalEntityTypeAnnotations relational)
+            {
+                relational.TableName = table;
+            }
+        }
+
+        [Obsolete("This method is not recommended, please use GetPagedList or GetPagedListAsync methods")]
+        public IQueryable<TEntity> GetAll()
+        {
+            return DbSet;
+        }
+
+        public IPagedList<TEntity> GetPagedList(
             Expression<Func<TEntity, bool>> predicate = null,
             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
             Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
-            bool disableTracking = false)
+            int pageIndex = 0,
+            int pageSize = 20,
+            bool disableTracking = false
+            )
         {
             IQueryable<TEntity> query = DbSet;
             if (disableTracking)
@@ -45,17 +65,154 @@ namespace Kodkod.EntityFramework.Repositories
 
             if (orderBy != null)
             {
-                return await orderBy(query).ToListAsync();
+                return orderBy(query).ToPagedList(pageIndex, pageSize);
             }
 
-            return await query.ToListAsync();
+            return query.ToPagedList(pageIndex, pageSize);
+        }
+
+        public Task<IPagedList<TEntity>> GetPagedListAsync(
+            Expression<Func<TEntity, bool>> predicate = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+            int pageIndex = 0,
+            int pageSize = 20,
+            bool disableTracking = false,
+            CancellationToken cancellationToken = default(CancellationToken)
+            )
+        {
+            IQueryable<TEntity> query = DbSet;
+            if (disableTracking)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (include != null)
+            {
+                query = include(query);
+            }
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToPagedListAsync(pageIndex, pageSize, 0, cancellationToken);
+            }
+
+            return query.ToPagedListAsync(pageIndex, pageSize, 0, cancellationToken);
+        }
+
+        public IPagedList<TResult> GetPagedList<TResult>(
+            Expression<Func<TEntity, TResult>> selector,
+            Expression<Func<TEntity, bool>> predicate = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+            int pageIndex = 0,
+            int pageSize = 20,
+            bool disableTracking = false
+            )
+            where TResult : class
+        {
+            IQueryable<TEntity> query = DbSet;
+            if (disableTracking)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (include != null)
+            {
+                query = include(query);
+            }
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).Select(selector).ToPagedList(pageIndex, pageSize);
+            }
+
+            return query.Select(selector).ToPagedList(pageIndex, pageSize);
+        }
+
+        public Task<IPagedList<TResult>> GetPagedListAsync<TResult>(
+            Expression<Func<TEntity, TResult>> selector,
+            Expression<Func<TEntity, bool>> predicate = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+            int pageIndex = 0,
+            int pageSize = 20,
+            bool disableTracking = false,
+            CancellationToken cancellationToken = default(CancellationToken)
+            )
+            where TResult : class
+        {
+            IQueryable<TEntity> query = DbSet;
+            if (disableTracking)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (include != null)
+            {
+                query = include(query);
+            }
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).Select(selector).ToPagedListAsync(pageIndex, pageSize, 0, cancellationToken);
+            }
+
+            return query.Select(selector).ToPagedListAsync(pageIndex, pageSize, 0, cancellationToken);
+        }
+
+        public TEntity GetFirstOrDefault(
+            Expression<Func<TEntity, bool>> predicate = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+            bool disableTracking = false
+            )
+        {
+            IQueryable<TEntity> query = DbSet;
+            if (disableTracking)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (include != null)
+            {
+                query = include(query);
+            }
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).FirstOrDefault();
+            }
+
+            return query.FirstOrDefault();
         }
 
         public async Task<TEntity> GetFirstOrDefaultAsync(
             Expression<Func<TEntity, bool>> predicate = null,
             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
             Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
-            bool disableTracking = false)
+            bool disableTracking = false
+            )
         {
             IQueryable<TEntity> query = DbSet;
             if (disableTracking)
@@ -80,5 +237,147 @@ namespace Kodkod.EntityFramework.Repositories
 
             return await query.FirstOrDefaultAsync();
         }
+
+        public TResult GetFirstOrDefault<TResult>(
+            Expression<Func<TEntity, TResult>> selector,
+            Expression<Func<TEntity, bool>> predicate = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+            bool disableTracking = false
+            )
+        {
+            IQueryable<TEntity> query = DbSet;
+            if (disableTracking)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (include != null)
+            {
+                query = include(query);
+            }
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).Select(selector).FirstOrDefault();
+            }
+
+            return query.Select(selector).FirstOrDefault();
+        }
+
+        public async Task<TResult> GetFirstOrDefaultAsync<TResult>(
+            Expression<Func<TEntity, TResult>> selector,
+            Expression<Func<TEntity, bool>> predicate = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
+            bool disableTracking = false
+            )
+        {
+            IQueryable<TEntity> query = DbSet;
+            if (disableTracking)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (include != null)
+            {
+                query = include(query);
+            }
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if (orderBy != null)
+            {
+                return await orderBy(query).Select(selector).FirstOrDefaultAsync();
+            }
+
+            return await query.Select(selector).FirstOrDefaultAsync();
+        }
+
+        public IQueryable<TEntity> FromSql(string sql, params object[] parameters) => DbSet.FromSql(sql, parameters);
+
+        public TEntity Find(params object[] keyValues) => DbSet.Find(keyValues);
+
+        public Task<TEntity> FindAsync(params object[] keyValues) => DbSet.FindAsync(keyValues);
+
+        public Task<TEntity> FindAsync(object[] keyValues, CancellationToken cancellationToken) => DbSet.FindAsync(keyValues, cancellationToken);
+
+        public int Count(Expression<Func<TEntity, bool>> predicate = null)
+        {
+            if (predicate == null)
+            {
+                return DbSet.Count();
+            }
+
+            return DbSet.Count(predicate);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public void Insert(params TEntity[] entities) => DbSet.AddRange(entities);
+
+        public void Insert(IEnumerable<TEntity> entities) => DbSet.AddRange(entities);
+
+        public Task InsertAsync(TEntity entity, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return DbSet.AddAsync(entity, cancellationToken);
+        }
+
+        public Task InsertAsync(params TEntity[] entities) => DbSet.AddRangeAsync(entities);
+
+        public Task InsertAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default(CancellationToken)) => DbSet.AddRangeAsync(entities, cancellationToken);
+
+        public void Update(TEntity entity)
+        {
+            DbSet.Update(entity);
+        }
+
+        public void UpdateAsync(TEntity entity)
+        {
+            DbSet.Update(entity);
+        }
+
+        public void Update(params TEntity[] entities) => DbSet.UpdateRange(entities);
+
+        public void Update(IEnumerable<TEntity> entities) => DbSet.UpdateRange(entities);
+
+        public void Delete(TEntity entity) => DbSet.Remove(entity);
+
+        public void Delete(object id)
+        {
+            // using a stub entity to mark for deletion
+            var typeInfo = typeof(TEntity).GetTypeInfo();
+            var key = DbContext.Model.FindEntityType(typeInfo).FindPrimaryKey().Properties.FirstOrDefault();
+            var property = typeInfo.GetProperty(key?.Name);
+            if (property != null)
+            {
+                var entity = Activator.CreateInstance<TEntity>();
+                property.SetValue(entity, id);
+                DbContext.Entry(entity).State = EntityState.Deleted;
+            }
+            else
+            {
+                var entity = DbSet.Find(id);
+                if (entity != null)
+                {
+                    Delete(entity);
+                }
+            }
+        }
+
+        public void Delete(params TEntity[] entities) => DbSet.RemoveRange(entities);
+
+        public void Delete(IEnumerable<TEntity> entities) => DbSet.RemoveRange(entities);
     }
 }
