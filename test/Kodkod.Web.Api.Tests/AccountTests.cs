25e3746d5dc6d4ef2fee6a1721905ea27b592f48
alirizaadiyahsi <alirizaadiyahsi@outlook.com>
2018-05-30 21:00:20 +0300

    Created a base test class and moved shared code into it

--- test/Kodkod.Web.Api.Tests/AccountTests.cs
+++ test/Kodkod.Web.Api.Tests/AccountTests.cs
@@ -1,59 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
-using AutoMapper;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace Kodkod.Web.Api.Tests
 {
-    public class AccountTests
+    public class AccountTests : ApiTestBase
     {
-        private readonly HttpClient _client;
-
-        private static readonly Dictionary<string, string> TestUserFormData = new Dictionary<string, string>
-        {
-            {"email", "testuser@mail.com"},
-            {"username", "testuser"},
-            {"password", "123qwe"}
-        };
-
-        public AccountTests()
-        {
-            ServiceCollectionExtensions.UseStaticRegistration = false;
-            var server = new TestServer(
-                new WebHostBuilder()
-                    .UseStartup<Startup>()
-                    .ConfigureAppConfiguration(config =>
-                    {
-                        config.SetBasePath(Path.GetFullPath(@"../../.."));
-                        config.AddJsonFile("appsettings.json", false);
-                    })
-            );
-
-            _client = server.CreateClient();
-        }
-
         [Fact]
         public async Task TestUnAuthorizedAccessAsync()
         {
-            var response = await _client.GetAsync("/api/test/AuthorizedGet");
+            var response = await Client.GetAsync("/api/test/AuthorizedGet");
             Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
         }
 
         [Fact]
         public async Task TestGetTokenAsync()
         {
-            var response = await LoginAsync();
+            var response = await LoginAsTestUserAsync();
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
             var responseString = await response.Content.ReadAsStringAsync();
@@ -64,30 +30,17 @@ namespace Kodkod.Web.Api.Tests
         [Fact]
         public async Task TestAuthorizedAccessAsync()
         {
-            var response = await LoginAsync();
+            var response = await LoginAsTestUserAsync();
             var responseJson = JObject.Parse(await response.Content.ReadAsStringAsync());
             var token = (string)responseJson["token"];
 
             var requestMessage = new HttpRequestMessage(HttpMethod.Get, "/api/test/AuthorizedGet/5");
             requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            var getValueResponse = await _client.SendAsync(requestMessage);
+            var getValueResponse = await Client.SendAsync(requestMessage);
             Assert.Equal(HttpStatusCode.OK, getValueResponse.StatusCode);
 
             var getValueResponseString = await getValueResponse.Content.ReadAsStringAsync();
             Assert.True(getValueResponseString == "value");
-        }
-
-        private static StringContent GetTestUserStringContent()
-        {
-            var bodyString = JsonConvert.SerializeObject(TestUserFormData);
-
-            return new StringContent(bodyString, Encoding.UTF8, "application/json");
-        }
-
-        //todo: move this to a base class to share
-        private async Task<HttpResponseMessage> LoginAsync()
-        {
-            return await _client.PostAsync("/api/account/login", GetTestUserStringContent());
         }
     }
 }
