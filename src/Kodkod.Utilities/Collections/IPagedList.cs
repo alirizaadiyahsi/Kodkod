5b9edfd8a1bd51da169ffd002bb9044c3b1e9754
alirizaadiyahsi <alirizaadiyahsi@outlook.com>
2018-06-02 02:49:29 +0300

    Added PagedList classes

---
+++ src/Kodkod.Utilities/Collections/IPagedList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Kodkod.Utilities.Collections
+{
+    public interface IPagedList<T>
+    {
+        int IndexFrom { get; }
+
+        int PageIndex { get; }
+
+        int PageSize { get; }
+
+        int TotalCount { get; }
+
+        int TotalPages { get; }
+
+        IList<T> Items { get; }
+
+        bool HasPreviousPage { get; }
+
+        bool HasNextPage { get; }
+    }
+}
---
+++ src/Kodkod.Utilities/Collections/PagedList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Kodkod.Utilities.Collections
+{
+    public class PagedList<T> : IPagedList<T>
+    {
+        public PagedList()
+        {
+            Items = new List<T>();
+        }
+
+        public int PageIndex { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int TotalCount { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public int IndexFrom { get; set; }
+
+        public IList<T> Items { get; set; }
+
+        public bool HasPreviousPage => PageIndex - IndexFrom > 0;
+
+        public bool HasNextPage => PageIndex - IndexFrom + 1 < TotalPages;
+    }
+}
