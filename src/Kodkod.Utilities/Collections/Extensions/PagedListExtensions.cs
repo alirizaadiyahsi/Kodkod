20d8093c3a6a3e8f0eb680c1ce5266a4cf913c3b
alirizaadiyahsi <alirizaadiyahsi@outlook.com>
2018-06-02 02:49:42 +0300

    PagedList extensions

---
+++ src/Kodkod.Utilities/Collections/Extensions/PagedListExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kodkod.Utilities.Collections.Extensions
+{
+    public static class PagedListExtensions
+    {
+        public static async Task<IPagedList<T>> ToPagedListAsync<T>(
+            this IQueryable<T> source,
+            int pageIndex,
+            int pageSize,
+            int indexFrom = 0,
+            CancellationToken cancellationToken = default(CancellationToken)
+        )
+        {
+            if (indexFrom > pageIndex)
+            {
+                throw new ArgumentException($"indexFrom: {indexFrom} > pageIndex: {pageIndex}, must indexFrom <= pageIndex");
+            }
+
+            var count = await source.CountAsync(cancellationToken).ConfigureAwait(false);
+            var items = await source.Skip((pageIndex - indexFrom) * pageSize)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken)
+                .ConfigureAwait(false);
+
+            var pagedList = new PagedList<T>
+            {
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                IndexFrom = indexFrom,
+                TotalCount = count,
+                Items = items,
+                TotalPages = (int)Math.Ceiling(count / (double)pageSize)
+            };
+
+            return pagedList;
+        }
+    }
+}
