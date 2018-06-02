f0c836fe6e2c61cced8afc8e86dc6fb62232ef17
alirizaadiyahsi <alirizaadiyahsi@outlook.com>
2018-06-02 03:01:38 +0300

    Added Test project for utilities

---
+++ test/Kodkod.Utilities.Tests/Collections/PageListTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Kodkod.EntityFramework;
+using Kodkod.Tests.Shared;
+using Kodkod.Utilities.Collections.PagedList.Extensions;
+using Xunit;
+
+namespace Kodkod.Utilities.Tests.Collections
+{
+    public class PageListTests : TestBase
+    {
+        private readonly KodkodDbContext _kodkodDbContext = GetInitializedDbContext();
+
+        [Fact]
+        public async Task ToPagedListAsyncTest()
+        {
+            var page = await _kodkodDbContext.Users.Where(u => u.UserName != null).ToPagedListAsync(1, 2);
+            var totalUserCount = _kodkodDbContext.Users.Count();
+
+            Assert.NotNull(page);
+            Assert.Equal(totalUserCount, page.TotalCount);
+            Assert.Equal(2, page.Items.Count);
+            Assert.Equal("C", page.Items[0].UserName);
+
+            page = await _kodkodDbContext.Users.Where(u => u.UserName != null).ToPagedListAsync(0, 2);
+
+            Assert.NotNull(page);
+            Assert.Equal(totalUserCount, page.TotalCount);
+            Assert.Equal(2, page.Items.Count);
+            Assert.Equal("A", page.Items[0].UserName);
+        }
+    }
+}
