61f29a4f9435e0bf556eb6f01a2fa090ec2ed5ee
alirizaadiyahsi <alirizaadiyahsi@outlook.com>
2018-06-03 23:02:23 +0300

    Added unit test for action filter

--- test/Kodkod.Web.Api.Tests/ActionFiltersTest.cs
+++ test/Kodkod.Web.Api.Tests/ActionFiltersTest.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using Kodkod.Core.Roles;
 using Kodkod.Web.Core.ActionFilters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.Filters;
-using NSubstitute;
+using Microsoft.AspNetCore.Routing;
 using Xunit;
 
 namespace Kodkod.Web.Api.Tests
@@ -13,11 +17,12 @@ namespace Kodkod.Web.Api.Tests
         public void TestKodkodDbContextActionFilter()
         {
             var kodkodDbContextActionFilter = new KodkodDbContextActionFilter(KodkodInMemoryContext);
-            var actionExecutingContext = Substitute.For<ActionExecutedContext>();
+            var actionContext = new ActionContext(new DefaultHttpContext(),new RouteData(), new ActionDescriptor() );
+            var actionExecutedContext = new ActionExecutedContext(actionContext,new List<IFilterMetadata>(), null);
             var testRole = new Role { Name = "test_role", Id = Guid.NewGuid() };
 
             KodkodInMemoryContext.Roles.Add(testRole);
-            kodkodDbContextActionFilter.OnActionExecuted(actionExecutingContext);
+            kodkodDbContextActionFilter.OnActionExecuted(actionExecutedContext);
 
             var insertedTestRole = KodkodInMemoryContext.Roles.Find(testRole.Id);
             Assert.Equal(testRole.Name, insertedTestRole.Name);
