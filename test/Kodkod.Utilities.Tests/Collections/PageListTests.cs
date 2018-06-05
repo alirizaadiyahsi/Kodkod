36a6ded2cda5edb0baf9f97651d35920e1044aae
alirizaadiyahsi <alirizaadiyahsi@outlook.com>
2018-06-06 00:45:22 +0300

    Added tests for queryable extensions

--- test/Kodkod.Utilities.Tests/Collections/PageListTests.cs
+++ test/Kodkod.Utilities.Tests/Collections/PageListTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Threading.Tasks;
-using Kodkod.Utilities.Linq.Extensions;
 using Kodkod.Utilities.PagedList.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
@@ -9,21 +8,15 @@ namespace Kodkod.Utilities.Tests.Collections
     public class PageListTests : UtilitiesTestBase
     {
         [Fact]
-        public async Task ToPagedListAsyncTest()
+        public async Task ToPagedListTest()
         {
             var users = KodkodInMemoryContext.Users;
             var usersCount = await users.CountAsync();
-            var pagedUserList = users.PagedBy(1, 2).ToPagedList(usersCount);
+            var pagedUserList = users.ToPagedList(usersCount);
 
             Assert.NotNull(pagedUserList);
             Assert.Equal(usersCount, pagedUserList.TotalCount);
-            Assert.Equal(2, pagedUserList.Items.Count);
-
-            pagedUserList = users.PagedBy(0, 3).ToPagedList(usersCount);
-
-            Assert.NotNull(pagedUserList);
-            Assert.Equal(usersCount, pagedUserList.TotalCount);
-            Assert.Equal(3, pagedUserList.Items.Count);
+            Assert.Equal(usersCount, pagedUserList.Items.Count);
         }
     }
 }
---
+++ test/Kodkod.Utilities.Tests/Linq/QueryableTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Kodkod.Utilities.Linq.Extensions;
+using Xunit;
+
+namespace Kodkod.Utilities.Tests.Linq
+{
+    public class QueryableTests : UtilitiesTestBase
+    {
+        [Fact]
+        public void ToPagedListAsyncTest()
+        {
+            var users = KodkodInMemoryContext.Users;
+            var pagedUserList = users.PagedBy(0, 2);
+
+            Assert.NotNull(pagedUserList);
+            Assert.Equal(2, pagedUserList.Count());
+        }
+    }
+}
