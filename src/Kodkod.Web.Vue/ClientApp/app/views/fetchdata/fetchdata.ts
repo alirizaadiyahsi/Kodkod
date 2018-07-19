3fde71b6ceaf3e9650e47449abd6b88a5e1bdff1
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2018-07-19 21:00:36 +0300

    Moved weatherforecast class to models folder

--- src/Kodkod.Web.Vue/ClientApp/app/views/fetchdata/fetchdata.ts
+++ src/Kodkod.Web.Vue/ClientApp/app/views/fetchdata/fetchdata.ts
@@ -1,13 +1,6 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 
-interface IWeatherForecast {
-    dateFormatted: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
-}
-
 @Component
 export default class FetchDataComponent extends Vue {
     forecasts: IWeatherForecast[] = [];
